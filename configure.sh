caaf4898b6f92f77e5f43df5dd68ca5d7e94196d
Eduardo Bortolini <ebortolini@gmail.com>
2013-02-12 23:22:00 -0200

    working in configuration script

--- configure.sh
+++ configure.sh
@@ -1,18 +1,57 @@
 #!/bin/bash
-EXIT=0
-CheckIfExist(){
-    if [ -d $Monitor ] 
+EXIT="y"
+CONF_FILE="MonitorB.cfg"
+CheckIfDirExist(){
+    if [ -d $1 ] 
     then
-	echo "directory"
+	retureturnedVal=1
     else
-	echo "not"
+	retureturnedVal=0
     fi
+	return $retureturnedVal
 }
-while [ $EXIT -ne 1 ]
+
+CheckIfFileExist(){
+    if [ -f $1 ] 
+    then
+	retureturnedVal=1
+    else
+	retureturnedVal=0
+    fi
+	return $retureturnedVal
+}
+
+CreateConfigurationFile(){
+	touch $CONF_FILE
+}
+
+CheckIfFileExist "MonitorB.cfg"
+if [ "$?" -eq 1 ]
+	then
+	read -p "File Exists. Would you like to substitute for a new file? (y/N) :" opt
+	if [ $opt = "N" ]; then
+		exit 1;
+	else
+		rm -f $CONF_FILE
+		CreateConfigurationFile
+	fi
+else
+	echo "Not Exists"
+	CreateConfigurationFile
+fi
+
+while [ $EXIT = "y" ]
 do
-    echo "Please, inform a directory/file to monitor"
+    echo "Please, inform a directory/file to monitor: "
     read Monitor
-    CheckIfExist "$Monitor"
-    echo "input from user: $Monitor"
+    CheckIfDirExist "$Monitor"
+    if [ "$?" -eq 1 ]
+	then
+	echo "Directory exists"
+	else
+	echo "Not Exists"
+    fi
+    #echo "input from user: $Monitor"
+    echo "More dirs ?  (y/N) :"
     read EXIT
 done
